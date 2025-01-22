c0b724caca85c52853bfa971194ba556dcee3ecb
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-01-22 15:04:10 +0500

    1. Making the crypto app

--- lib/Views/SplashScreen/SplashScreen.dart
+++ lib/Views/SplashScreen/SplashScreen.dart
@@ -12,6 +12,20 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:Container(
+        child: Column(
+          children: [
+            Image.asset('assets/images/1.blue.jpg'),
+            Text('The Future',style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold ),),
+            Text('Learn more about cryptocurrency ,look to',style: TextStyle(fontSize: 20,fontWeight:FontWeight.normal,color: Colors.grey ),),
+            Text('The Future in ID Crypto',style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold ),),
+          ],
+        ),
+      ) ,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:crypto_app/Views/HomeScreen/HomeScreen.dart';
 import 'package:crypto_app/Views/SplashScreen/SplashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
