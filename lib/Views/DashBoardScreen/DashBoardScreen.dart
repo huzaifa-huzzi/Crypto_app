c6374d5eba91c2817ec66b1404672fbf4ff177df
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-01-22 15:30:08 +0500

    1. Making the crypto app

---
+++ lib/Views/DashBoardScreen/DashBoardScreen.dart
@@ -0,0 +1,68 @@
+import 'package:crypto_app/Resources/Colors/Colors.dart';
+import 'package:crypto_app/Views/HomeScreen/HomeScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  final PersistentTabController _controller = PersistentTabController(initialIndex: 0);
+
+  List<Widget> _buildScreens() {
+    return [
+      HomeScreen(),
+      HomeScreen(),
+      HomeScreen(),
+      HomeScreen(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.home, size: 40),
+        activeColorPrimary: AppColors.primary,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.explore, size: 40),
+        activeColorPrimary: AppColors.primary,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.notifications, size: 40),
+        activeColorPrimary: AppColors.primary,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.person, size: 40),
+        activeColorPrimary: AppColors.primary,
+        inactiveColorPrimary: Colors.grey,
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        backgroundColor: Colors.white,
+        navBarStyle: NavBarStyle.style6,
+        padding:const EdgeInsets.only(left: 20,right: 20,bottom: 20),
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(50.0),
+          colorBehindNavBar: Colors.white,
+        ),
+      ),
+    );
+  }
+}
--- lib/Views/SplashScreen/SplashScreen.dart
+++ lib/Views/SplashScreen/SplashScreen.dart
@@ -1,4 +1,7 @@
+import 'package:crypto_app/Resources/Colors/Colors.dart';
+import 'package:crypto_app/Views/DashBoardScreen/DashBoardScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 
 
@@ -19,10 +22,43 @@ class _SplashScreenState extends State<SplashScreen> {
       body:Container(
         child: Column(
           children: [
+            SizedBox(height: height * 0.06,),
             Image.asset('assets/images/1.blue.jpg'),
-            Text('The Future',style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold ),),
-            Text('Learn more about cryptocurrency ,look to',style: TextStyle(fontSize: 20,fontWeight:FontWeight.normal,color: Colors.grey ),),
-            Text('The Future in ID Crypto',style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold ),),
+            SizedBox(height: height * 0.03,),
+            Column(
+              children: [
+                Text('The Future',style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold ),),
+                Text('Learn more about cryptocurrency ,look to',style: TextStyle(fontSize: 20,fontWeight:FontWeight.normal,color: Colors.grey ),),
+                Text('The Future in ID Crypto',style: TextStyle(fontSize: 20,fontWeight:FontWeight.normal,color: Colors.grey),),
+              ],
+            ),
+            SizedBox(height: height * 0.08,),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.14),
+              child: InkWell(
+                onTap: (){
+                  Get.to(() => DashboardScreen());
+                },
+                child: Container(
+                   decoration: BoxDecoration(
+                     color: AppColors.primary,
+                     borderRadius: BorderRadius.circular(50),
+                   ),
+                  child: Padding(
+                      padding:EdgeInsets.symmetric(horizontal: width * 0.05 ,vertical: height *0.013),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                         Text('CREATE PORTFOLIO',style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal),),
+                        RotationTransition(turns: AlwaysStoppedAnimation(310/360),
+                        child: Icon(Icons.arrow_forward_rounded),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ) ,
