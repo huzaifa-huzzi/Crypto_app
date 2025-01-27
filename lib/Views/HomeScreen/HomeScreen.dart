0b7731f61d31935ec887904f6da38ae1d109a66b
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-01-27 16:21:13 +0500

    1. Making the crypto app

--- lib/Views/HomeScreen/HomeScreen.dart
+++ lib/Views/HomeScreen/HomeScreen.dart
@@ -12,7 +12,6 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
