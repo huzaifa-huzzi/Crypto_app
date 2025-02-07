3a8598480494b5fe1cd3aeed1cb71d042e814bb4
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-02-07 15:16:51 +0500

    1. Making the crypto app

---
+++ lib/View_Model/Controllers/Crypto_Controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+class SelectCoinController extends GetxController {
+  var selectedTabIndex = 2.obs;
+
+
+  final List<String> tabs = ['D', 'W', 'M', '3M', '6M', 'Y'];
+
+  void updateSelectedTab(int index) {
+    selectedTabIndex.value = index;
+  }
+}
--- lib/Views/SelecCoin/SelectCoin.dart
+++ lib/Views/SelecCoin/SelectCoin.dart
@@ -1,8 +1,8 @@
+import 'package:crypto_app/View_Model/Controllers/Crypto_Controller.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:crypto_app/Models/ChartModel.dart';
-import 'package:crypto_app/View_Model/crypto_view_Model.dart';
+import 'package:get/get.dart';
 import '../../Resources/Colors/Colors.dart';
+
 
 class SelectCoin extends StatefulWidget {
   final String image, title, symbol, currentPrice, marketChangePrice24H, low24H, high24H, totalVolume;
@@ -24,8 +24,7 @@ class SelectCoin extends StatefulWidget {
 }
 
 class _SelectCoinState extends State<SelectCoin> {
-
-  List<String> text = ['D','W','M','3M','6M','Y'];
+  final SelectCoinController controller = Get.put(SelectCoinController());
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +38,7 @@ class _SelectCoinState extends State<SelectCoin> {
           width: width,
           child: Column(
             children: [
+              // Header Section
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: width * 0.05, vertical: height * 0.02),
                 child: Row(
@@ -46,23 +46,14 @@ class _SelectCoinState extends State<SelectCoin> {
                   children: [
                     Row(
                       children: [
-                        SizedBox(
-                          height: height * 0.08,
-                          child: Image.network(widget.image),
-                        ),
+                        SizedBox(height: height * 0.08, child: Image.network(widget.image)),
                         SizedBox(width: width * 0.02),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              widget.title,
-                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
+                            Text(widget.title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                             SizedBox(height: height * 0.01),
-                            Text(
-                              widget.symbol,
-                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.normal, color: Colors.grey),
-                            ),
+                            Text(widget.symbol, style: const TextStyle(fontSize: 20, color: Colors.grey)),
                           ],
                         ),
                       ],
@@ -70,16 +61,12 @@ class _SelectCoinState extends State<SelectCoin> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Text(
-                          '\$${widget.currentPrice}',
-                          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.normal, color: Colors.black),
-                        ),
+                        Text('\$${widget.currentPrice}', style: const TextStyle(fontSize: 20)),
                         SizedBox(height: height * 0.01),
                         Text(
                           '${widget.marketChangePrice24H}%',
                           style: TextStyle(
                             fontSize: 16,
-                            fontWeight: FontWeight.normal,
                             color: double.tryParse(widget.marketChangePrice24H) != null &&
                                 double.parse(widget.marketChangePrice24H) >= 0
                                 ? Colors.green
@@ -92,146 +79,40 @@ class _SelectCoinState extends State<SelectCoin> {
                 ),
               ),
               const Divider(),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width * 0.05, vertical: height * 0.02),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          const Text(
-                            'Low',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.grey),
-                          ),
-                          SizedBox(height: height * 0.01),
-                          Text(
-                            '\$${widget.low24H}',
-                            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: width * 0.05),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          const Text(
-                            'High',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.grey),
-                          ),
-                          SizedBox(height: height * 0.01),
-                          Text(
-                            '\$${widget.high24H}',
-                            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: width * 0.05),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          const Text(
-                            'Volume',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.grey),
-                          ),
-                          SizedBox(height: height * 0.01),
-                          Text(
-                            '\$${widget.totalVolume}',
-                            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              FutureBuilder<List<ChartModel>>(
-                future: CryptoViewModel().fetchChartApi(),
-                builder: (BuildContext context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(color: AppColors.primary),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot.error}'),
-                    );
-                  } else if (snapshot.hasData) {
-                    final List<CandleData> candleData = snapshot.data!
-                        .map((data) => CandleData(
-                      data.timestamp.toDouble(),
-                      data.open,
-                      data.high,
-                      data.low,
-                      data.close,
-                    ))
-                        .toList();
-
-                    return SizedBox(
-                      height: height * 0.4,
-                      width: width,
-                      child: SfCartesianChart(
-                        primaryXAxis: NumericAxis(),
-                        primaryYAxis: NumericAxis(),
-                        series: <CandleSeries>[
-                          CandleSeries<CandleData, double>(
-                            dataSource: candleData,
-                            xValueMapper: (CandleData data, _) => data.timestamp,
-                            lowValueMapper: (CandleData data, _) => data.low,
-                            highValueMapper: (CandleData data, _) => data.high,
-                            openValueMapper: (CandleData data, _) => data.open,
-                            closeValueMapper: (CandleData data, _) => data.close,
-                            bullColor: Colors.green,
-                            bearColor: Colors.red,
-                          ),
-                        ],
-                      ),
-                    );
-                  } else {
-                    return const Center(
-                      child: Text('No data available'),
-                    );
-                  }
-                },
-              ),
-              Container(
+              // Tab Selection (Just switching between tabs without fetching data)
+              SizedBox(
                 height: height * 0.03,
-                child: ListView.builder(
+                child: Obx(() => ListView.builder(
                   shrinkWrap: true,
                   scrollDirection: Axis.horizontal,
-                  itemCount: text.length,
-                    itemBuilder: (context,index){
-                      return
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: width * 0.02),
-                            child: Container(
-                              padding: EdgeInsets.symmetric(horizontal: width * 0.01,vertical: height * 0.005),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: AppColors.primary.withOpacity(0.5)
-                              ),
-                              child: Text(text[index],style: TextStyle(fontSize: 18),),
-                            ),
-                          );
-                    }
-                ),
-              )
+                  itemCount: controller.tabs.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width * 0.02),
+                      child: GestureDetector(
+                        onTap: () => controller.updateSelectedTab(index),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: width * 0.01, vertical: height * 0.005),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: controller.selectedTabIndex.value == index
+                                ? AppColors.primary.withOpacity(0.5)
+                                : Colors.transparent,
+                          ),
+                          child: Text(
+                            controller.tabs[index],
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                )),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-}
-
-class CandleData {
-  final double timestamp;
-  final double open;
-  final double high;
-  final double low;
-  final double close;
-
-  CandleData(this.timestamp, this.open, this.high, this.low, this.close);
 }
