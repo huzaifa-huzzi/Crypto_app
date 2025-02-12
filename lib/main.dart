0745c7252d7a964e58b75af46be714da4bd50a9b
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-02-12 18:12:10 +0500

    1. Making the crypto app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:crypto_app/Views/SplashScreen/SplashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+
+
 void main() {
   runApp(const MyApp());
 }
